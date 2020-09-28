b36a1ef4237946505179116af6347553bb11d87f
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-28 15:48:20 -0500

    feat: Syntax, Types, and Variables #1 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World!");
+        System.out.println("");
+
+        int myFavoriteNumber = 12;
+        System.out.println(myFavoriteNumber);
+
+//    String myString = "This is my favorite string";
     }
+
+
 }
