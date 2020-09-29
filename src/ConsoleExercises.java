8fb8bcdf28aaf2649a1ee354bfc0054cd4276c21
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-29 13:56:27 -0500

    feat: scanner #1 complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
+        // Number output formatting exercise
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f %n", pi);
+
+        // Scanner class exercises
+
+        // First import the scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Prompt the console for input and store it as a variable
+        System.out.println("Enter an integer of your choosing: ");
+        int userInput = scan.nextInt();
+
+        // Output the varible back to the user
+        System.out.printf("You entered: %d", userInput);
+
+        // How to handle error output if there is a problem with the entered data?  (research TODO)
+
+        
     }
 }
