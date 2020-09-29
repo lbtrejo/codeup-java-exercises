bbbde3f6d61d27bd1e13c72b4d272e8f7937276c
Levi Trejo <levi.b.trejo@gmail.com>
2020-09-29 14:17:38 -0500

    feat: scanner #3 and #4 complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -12,28 +12,35 @@ public class ConsoleExercises {
         Scanner scan = new Scanner(System.in);
 
         // Prompt the console for input and store it as a variable
-        System.out.println("Enter an integer of your choosing: ");
-        int userInput = scan.nextInt();
-
-        // Output the varible back to the user
-        System.out.printf("You entered: %d%n", userInput);
+//        System.out.println("Enter an integer of your choosing: ");
+//        int userInput = scan.nextInt();
+//
+//        // Output the varible back to the user
+//        System.out.printf("You entered: %d%n", userInput);
 
         // How to handle error output if there is a problem with the entered data?  (research TODO)
 
         // Scanner 2:
         // Prompt the user:
-        System.out.println("Enter 3 words that describe you: ");
-
-        String firstWord = scan.next();
-        String secondWord = scan.next();
-        String thirdWord = scan.next();
-
-        System.out.println("Here is your description: ");
-        System.out.printf("First descriptor: %s%n", firstWord);
-        System.out.printf("Second descriptor: %s%n", secondWord);
-        System.out.printf("Third descriptor: %s%n", thirdWord);
+//        System.out.println("Enter 3 words that describe you: ");
+//
+//        String firstWord = scan.next();
+//        String secondWord = scan.next();
+//        String thirdWord = scan.next();
+//
+//        System.out.println("Here is your description: ");
+//        System.out.printf("First descriptor: %s%n", firstWord);
+//        System.out.printf("Second descriptor: %s%n", secondWord);
+//        System.out.printf("Third descriptor: %s%n", thirdWord);
 
         // Entering fewer than 3 words doesn't allow the program to complete, it hangs waiting for the last input
         // Entering more than 3 words only returns the first 3 words in the sequence, anything additional is dropped
+
+        // Scanner #3:
+
+        System.out.println("Enter a sentence: ");
+        //String userSentence = scan.next();  // Only returns the first word of the sentence
+        String userSentence = scan.nextLine(); // Returns the full sentence as the string variable
+        System.out.printf("Here is your sentence: \"%s\"%n", userSentence);
     }
 }
