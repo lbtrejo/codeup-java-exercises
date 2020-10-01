61bdfea685e0b33a40312e5d959a5d4f14427d01
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-01 11:54:30 -0500

    feat: solution to 1a

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,15 @@
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+        // 1.  Loop Basics
+            // a. While loop
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.printf("%s ", i);
+            i++;
+        }
+
+            // b. Do While
+        
+    }
 }
