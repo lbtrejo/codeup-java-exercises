fbadb4cd527a67887012904234d270295e5b585b
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-02 17:27:23 -0500

    feat: exercise #2 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
 //        add(12, 3);
@@ -7,6 +9,9 @@ public class MethodsExercises {
 //        modulus(12, 5);
 //        longMultiply(-2,5);
         System.out.println(recursiveMultiply(6, 5));
+
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
     }
 
     // 1.  Basic arithmetic
@@ -31,8 +36,6 @@ public class MethodsExercises {
         return num1 % num2;
     }
     public static int longMultiply(int num1, int num2) {
-        // 3 * 4
-        // 3 + 3 + 3 + 3
         int result = 0;
         for (int x = 1; x <= num2; x++) {
             result  += num1;
@@ -48,5 +51,21 @@ public class MethodsExercises {
             result = num1 + recursiveMultiply(num1, num2 - 1);
         }
         return result;
+    }
+
+    public static int getInteger(int min, int max) {
+        // scan for user input
+        Scanner scanner = new Scanner(System.in);
+
+        int currentInput = scanner.nextInt();
+        if (min <= currentInput && currentInput <= max) {
+            return currentInput;
+        } else {
+            System.out.println("Please enter a valid integer");
+            return getInteger(min, max);
+        }
+        // verify the input is inside the valid range
+        // return the number if valid
+        // rerun the method if invalid
     }
 }
