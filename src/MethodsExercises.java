177760198e275dc0dbb569c4b2501db06a89a13b
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-02 16:47:48 -0500

    feat: exercise #1 complete with bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,52 @@
 public class MethodsExercises {
-    
+    public static void main(String[] args) {
+//        add(12, 3);
+//        subtract(12, 3);
+//        multiply(12, 3);
+//        divide(12, 3);
+//        modulus(12, 5);
+//        longMultiply(-2,5);
+        System.out.println(recursiveMultiply(6, 5));
+    }
+
+    // 1.  Basic arithmetic
+    public static int add(int num1, int num2) {
+        System.out.println(num1 + num2);
+        return num1 + num2;
+    }
+    public static int subtract(int num1, int num2) {
+        System.out.println(num1 - num2);
+        return num1 - num2;
+    }
+    public static int multiply(int num1, int num2) {
+        System.out.println(num1 * num2);
+        return num1 * num2;
+    }
+    public static int divide(int num1, int num2) {
+        System.out.println(num1 / num2);
+        return num1 / num2;
+    }
+    public static int modulus(int num1, int num2) {
+        System.out.println(num1 % num2);
+        return num1 % num2;
+    }
+    public static int longMultiply(int num1, int num2) {
+        // 3 * 4
+        // 3 + 3 + 3 + 3
+        int result = 0;
+        for (int x = 1; x <= num2; x++) {
+            result  += num1;
+        }
+        System.out.println(result);
+        return result;
+    }
+    public static int recursiveMultiply(int num1, int num2) {
+        int result;
+        if (num2 == 1){
+            result = num1;
+        } else {
+            result = num1 + recursiveMultiply(num1, num2 - 1);
+        }
+        return result;
+    }
 }
