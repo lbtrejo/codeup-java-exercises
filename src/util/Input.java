f9e2dfa6d98a8913ebaa80d9e12a912ab00f2756
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-06 11:23:48 -0500

    feat: #3 complete, bonus up next

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,58 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+    private Scanner inputScan;
+
+    public Input(){
+       this.inputScan = new Scanner(System.in);
+    }
+
+    public String getString(){
+        System.out.println("Enter a string: ");
+        return this.inputScan.next();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Yes or no?: ");
+        String userInput = this.inputScan.next();
+        return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+    }
+
+    public int getInt(){
+        System.out.println("Enter an integer: ");
+        return this.inputScan.nextInt();
+    }
+
+    public int getInt(int min, int max){
+        System.out.printf("Enter an integer between %d and %d: %n", min, max);
+        int userInput = this.inputScan.nextInt();
+        if (min <= userInput && userInput <= max) {
+            System.out.printf("Your chosen integer: %d%n", userInput);
+            return userInput;
+        } else {
+            System.out.println("Please enter a valid integer");
+            return getInt(min, max);
+        }
+
+    }
+
+    public double getDouble(){
+        System.out.println("Enter an number: ");
+        return this.inputScan.nextDouble();
+    }
+
+    public double getDouble(double min, double max){
+        System.out.printf("Enter a number between %f and %f: %n", min, max);
+        double userInput = this.inputScan.nextDouble();
+        if (min <= userInput && userInput <= max) {
+            System.out.printf("Your chosen number: %f%n", userInput);
+            return userInput;
+        } else {
+            System.out.println("Please enter a valid number in the range: ");
+            return getDouble(min, max);
+        }
+
+    }
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,26 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+
+        String userInput;
+        Input testing = new Input();
+        userInput = testing.getString();
+        System.out.println("User Input: " + userInput);
+
+        boolean boolTest = testing.yesNo();
+        System.out.println("yesNo result: " + boolTest);
+
+        int intTest = testing.getInt();
+        System.out.println("getInt result: " + intTest);
+
+        int intTestParams = testing.getInt(1, 10);
+        System.out.println("getIntParams result: " + intTestParams);
+
+        double dblTest = testing.getDouble();
+        System.out.println("getDouble result: " + dblTest);
+
+        double dblTestParams = testing.getDouble(1.0, 10.0);
+        System.out.println("getDoubleParams result: " + dblTestParams);
+    }
+}
