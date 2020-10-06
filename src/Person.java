d80ce4c0c684f32b0ebdd11e01c40ad5d8d0ccbc
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-06 09:39:02 -0500

    feat: #1 Object Basics complete

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,29 @@
 public class Person {
-    
+    private String name;
+    public String getName(){
+//TODO: return the person's name
+        System.out.println(name);
+        return name;
+    }
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+        System.out.println("Name set as: " + this.name);
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + this.name);
+    }
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Person Levi = new Person("Levi");
+        Levi.getName();
+        Levi.sayHello();
+        Levi.setName("Levi Trejo");
+        Levi.sayHello();
+    }
 }
