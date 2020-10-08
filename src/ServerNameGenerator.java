ae59239df79573e92792e7322eeb3dc6dd5b3f77
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-08 09:35:47 -0500

    feat: refactor for style and clarity

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,8 @@
 public class ServerNameGenerator {
-    public static void main(String[] args) {
-        String[] alpha = {"pizza", "sandwich", "taco", "burger", "steak", "fries", "pasta", "cereal", "toast", "butter"};
-        String[] omega = {"agreeable", "ambitious", "brave", "calm", "delightful", "eager", "faithful", "gentle", "happy", "jolly"};
+    public static String[] alpha = {"pizza", "sandwich", "taco", "burger", "steak", "fries", "pasta", "cereal", "toast", "butter"};
+    public static String[] omega = {"agreeable", "ambitious", "brave", "calm", "delightful", "eager", "faithful", "gentle", "happy", "jolly"};
 
+    public static void main(String[] args) {
         System.out.println("Here is your server name:");
         System.out.printf("%s-%s", getRandomElement(omega), getRandomElement(alpha) );
         }
