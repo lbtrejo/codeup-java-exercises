380d38a31a23afe3e71c96dcd29ac0786df595a2
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-09 12:08:21 -0500

    feat: commenting code out for exercise #1

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,19 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
-
-    public Rectangle(int rLength, int rWidth){
-        this.length = rLength;
-        this.width = rWidth;
-    }
-
-    public int getArea(){
-        return this.length * this.width;
-    }
-
-    public int getPerimeter(){
-        return 2 * (this.length + this.width);
-    }
+//    protected int length;
+//    protected int width;
+//
+//    public Rectangle(int rLength, int rWidth){
+//        this.length = rLength;
+//        this.width = rWidth;
+//    }
+//
+//    public int getArea(){
+//        return this.length * this.width;
+//    }
+//
+//    public int getPerimeter(){
+//        return 2 * (this.length + this.width);
+//    }
 }
