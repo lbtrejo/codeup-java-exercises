226340dae6fa715ea6d5deab3f331b8a97c55061
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-13 12:02:46 -0500

    feat: initial files for Grades app exercise, student class fully built to solve for problem 1

---
+++ src/grades/Student.java
@@ -0,0 +1,47 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String studentName){
+        this.name = studentName;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        int gradeTotal = 0;
+        int numberOfGrades = 0;
+        for (Integer grade : grades){
+            gradeTotal += grade;
+            numberOfGrades++;
+        }
+        return (double)gradeTotal/numberOfGrades;
+    }
+
+
+    public static void main(String[] args) {
+        Student fer = new Student("Fer");
+        fer.addGrade(100);
+        fer.addGrade(23);
+        fer.addGrade(64);
+        fer.addGrade(97);
+        fer.addGrade(13);
+        fer.addGrade(13);
+
+
+        System.out.println("fer.getName() = " + fer.getName());
+        System.out.println("fer.getGradeAverage() = " + fer.getGradeAverage());
+    }
+}
